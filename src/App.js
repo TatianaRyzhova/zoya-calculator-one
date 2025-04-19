60c37c695d458cec62f5d016486740068b30da42
tatiana.ryzhova <tryzhova93@gmail.com>
2025-04-19 14:51:34 -0400

    update design

--- src/App.js
+++ src/App.js
@@ -1,222 +1,88 @@
 import React, { useState } from 'react';
 import './App.css';
-import ProbabilityFormula from './mathFormula';
-import Accordion from './components/Accordion';
 
 function App() {
-  const [number, setNumber] = useState({
-      one: '',
-      two: '',
-      three: '',
-      four: '',
+  const [values, setValues] = useState({
+    one: 0,
+    two: 0,
+    three: 0,
+    four: ''
   });
+  const [result, setResult] = useState(null);
+  const [risk, setRisk] = useState('');
 
-  const [result, setResult] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
 
-  const indicators = [
-    {
-      title: 'Показатель1',
-      description: 'Отягощенный акушерско-гинекологический анамнез (преждевременные роды в анамнезе, поздний выкидыш в анамнезе)',
-    },
-    {
-      title: 'Показатель2',
-      description: 'Дефект шейки матки (истмико-цервикальная недостаточность в предыдущую беременность, разрыв шейки матки в анамнезе, два и более инструментальных расширения цервикального канала в анамнезе, конизация или расширенная эксцизия шейки матки в анамнезе, лазерная вапоризация шейки матки в анамнезе)',
-    },
-    {
-      title: 'Показатель3',
-      description: 'Инфекционно-воспалительные заболевания и дисбиотические состояния при настоящей беременности (рецидивирующий бактериальный вагиноз, острый вагинит, цервицит, инфекции мочевыводящих путей, бессимптомная бактериурия)',
-    },
-    {
-      title: 'Показатель4',
-      description: 'Количество лейкоцитов в микроскопическом исследовании отделяемого из цервикального канала в I триместре настоящей беременности (количество клеток в поле зрения)',
-    },
-  ];
-
-
-  function handleChange(event) {
-      const input = event.target;
-      const name = input.name;
-      const value = input.value;
-      setNumber({
-        ...number,
-        [name]: value
-      });
+  const calculate = () => {
+    if (values.four === '' || values.five === '') {
+      alert('Пожалуйста, заполните все обязательные поля.');
+      return;
     }
 
-    function handleSubmit(event) {
-      event.preventDefault();
-      setSubmitted(true);
+    const z = (-3.601) + 1.044 * values.one + 1.502 * values.two + 0.581 * values.three + 0.217 * values.four;
+    const P = (1 / (1 + Math.exp(-z))) * 100;
+    setResult(P.toFixed(2));
+    setRisk(P > 41.3 ? 'Высокий риск' : 'Низкий риск');
+  };
 
-      const one = 1.44 * Number(number.one);
-      const two = 1.502 * Number(number.two);
-      const three = 0.581 * Number(number.three);
-      const four = 0.217 * Number(number.four);
-
-      const z = -3.601 + one + two + three + four;
-      const P = (1 / (1 + Math.exp(-z))) * 100;
-      setResult(P);
-
-      // setSubmitted(false);
-    }
-
-  
   return (
-      <div className="container">
-          <h1 className='title'>Прогнозирование в первом триместре беременности поздних преждевременных спонтанных родов</h1>
-          <p style={{ fontWeight: "bold" }}>Формула:</p>
-          {/* <p>Р = (1 / (1 + е<sup>-z</sup>)) x 100%</p> */}
-          {/* <p>z = (-3,601) + (1,044 × Показатель1) + (1,502 × Показатель2) + (0,581 × Показатель3) + (0,217 × Показатель4)</p> */}
-          <ProbabilityFormula />
-          <p>где:</p>
-          <p>z = (-3,601) + (1,044 × Показатель1) + (1,502 × Показатель2) + (0,581 × Показатель3) + (0,217 × Показатель4)</p>
-          {/* <ul>
-              <li>Р – вероятность НБ у женщин с ожирением (%)</li>
-                  <li>е – математическая постоянная = 2,718,</li>
-                  <li>Х<sub>КУР</sub> – наличие курения   (0 – курящие, 1 - некурящие)</li>
-                  <li>X<sub>AGT</sub> – концентрация в сыворотке крови ангиотензиногена (0 - менее 0,608, 1 – более 0,608)</li>
-                  <li>Х<sub>TTR</sub> –  концентрация в сыворотке крови транстиретина (0 - менее 0,074, 1 – более 0,074)</li>
-                  <li>Х<sub>С-пептид</sub> – концентрация в сыворотке крови С-пептида (0 - более 5,2 нг/мл, 1 – менее 5,2 нг/мл)</li>
-          </ul> */}
-          
-          <div className="accordion-container">
-            {indicators.map((indicator, index) => (
-              <Accordion key={index} title={indicator.title} content={<p>{indicator.description}</p>} />
-            ))}
-          </div>
+    <div className="container">
+      <h1 className='title'>Прогнозирование в первом триместре беременности поздних преждевременных спонтанных родов</h1>
+    
 
-          {/* <div>
-            <table>
-              <tbody>
-                <tr>
-                <th style={{width: "120px", fontWeight: "bold"}}>Показатель</th>
-                <th style={{width: "520px", fontWeight: "bold"}}>Название показателя</th>
-                <th style={{width: "120px", fontWeight: "bold"}}>Значение</th>
-                </tr>
-
-                <tr>
-                <th style={{width: "120px"}}>Показатель1</th>
-                <th style={{width: "520px"}}>Отягощенный акушерско-гинекологический анамнез (преждевременные роды в анамнезе, поздний выкидыш в анамнезе)</th>
-                <th style={{width: "120px"}}>
-                  <div>0 - Нет</div>
-                  <div>1 - Да</div>
-                </th>
-                </tr>
-
-                <tr>
-                <th style={{width: "120px"}}>Показатель2</th>
-                <th style={{width: "520px"}}>Дефект шейки матки (истмико-цервикальная недостаточность в предыдущую беременность, разрыв шейки матки в анамнезе, два и более инструментальных расширения цервикального канала в анамнезе, конизация или расширенная эксцизия шейки матки в анамнезе, лазерная вапоризация шейки матки в анамнезе)</th>
-                <th style={{width: "120px"}}>
-                  <div>0 - Нет</div>
-                  <div>1 - Да</div>
-                </th>
-                </tr>
-
-                <tr>
-                <th style={{width: "120px"}}>Показатель3</th>
-                <th style={{width: "520px"}}>Инфекционно-воспалительные заболевания и дисбиотические состояния при настоящей беременности (рецидивирующий бактериальный вагиноз, острый вагинит, цервицит, инфекции мочевыводящих путей, бессимптомная бактериурия)</th>
-                <th style={{width: "120px"}}>
-                  <div>0 - Нет</div>
-                  <div>1 - Да</div>
-                </th>
-                </tr>
-
-                <tr>
-                <th style={{width: "120px"}}>Показатель4</th>
-                <th style={{width: "520px"}}>Количество лейкоцитов в микроскопическом исследовании отделяемого из цервикального канала в I триместре настоящей беременности (количество клеток в поле зрения)</th>
-                <th style={{width: "120px"}}></th>
-                </tr>
-
-              </tbody>
-            </table>
-          </div> */}
-
-          <p style={{ fontWeight: "bold", paddingTop: "16px" }}>Интерпретация:</p>
-          <p>Более 0,413 — Высокий риск</p>
-          <p>0,413 и менее — Низкий риск </p>
-
-          <form onSubmit={handleSubmit}>
-              <div className="tables">
-                  <table>
-                      <tbody>
-                          <tr>
-                              <th style={{width: "220px"}}>Показатель1
-                                <div style={{ fontSize: "12px", color: "rgba(0, 0, 0, 0.4)" }}>0 - Нет, 1 - Да</div>
-                              </th>
-                              <th>
-                                  <input
-                                      type="number"
-                                      placeholder="0 или 1"
-                                      name="one"
-                                      onChange={handleChange}
-                                      value={number.one}
-                                      submitted={submitted}
-                                      required
-                                  />
-                              </th>
-                          </tr>
-                          <tr>
-                              <th style={{width: "220px"}}>Показатель2
-                                <div style={{ fontSize: "12px", color: "rgba(0, 0, 0, 0.4)" }}>0 - Нет, 1 - Да</div>
-                              </th>
-                              <th>
-                                  <input
-                                      type="number"
-                                      placeholder="0 или 1"
-                                      name="two"
-                                      onChange={handleChange}
-                                      value={number.two}
-                                      submitted={submitted}
-                                      required
-                                  />
-                              </th>
-                          </tr>
-                          <tr>
-                              <th style={{width: "220px"}}>Показатель3
-                                <div style={{ fontSize: "12px", color: "rgba(0, 0, 0, 0.4)" }}>0 - Нет, 1 - Да</div>
-                              </th>
-                              <th>
-                                  <input
-                                      type="number"
-                                      placeholder="0 или 1"
-                                      name="three"
-                                      onChange={handleChange}
-                                      value={number.three}
-                                      submitted={submitted}
-                                      required
-                                  />
-                              </th>
-                          </tr>
-                          <tr>
-                              <th style={{width: "220px"}}>Показатель4</th>
-                              <th>
-                                  <input
-                                      type="number"
-                                      // placeholder="0 или 1"
-                                      name="four"
-                                      onChange={handleChange}
-                                      value={number.four}
-                                      submitted={submitted}
-                                      required
-                                  />
-                              </th>
-                          </tr>
-                      </tbody>   
-                  </table>
-              </div>
-              <button type="submit">РАССЧИТАТЬ</button>
-          </form>
-          <div className="result-space" style={{ minHeight: '50px', paddingBottom: '30px' }}>
-              {submitted && (
-                  <div className="result">
-                  Результат:
-                  <span className={result >= 0.413 ? 'result-red' : 'result-green'}> {result}</span>
-                  <p className="information">{'Р > 0,413 - высокий риск'}</p>
-                  <p className="information">{'Р < 0,413 - низкий риск'}</p>
-              </div>
-              )}
-          </div>
+      <div className="form-group">
+        <label>
+          Отягощенный акушерско-гинекологический анамнез
+          <span className="description">(преждевременные роды в анамнезе, поздний выкидыш в анамнезе)</span>
+        </label>
+        <select name="one" value={values.one} onChange={handleChange}>
+          <option value={0}>Нет</option>
+          <option value={1}>Да</option>
+        </select>
       </div>
+
+      <div className="form-group">
+        <label>
+          Дефект шейки матки
+          <span className="description">(истмико-цервикальная недостаточность в предыдущую беременность, разрыв шейки матки в анамнезе, два и более инструментальных расширения цервикального канала в анамнезе, конизация или расширенная эксцизия шейки матки в анамнезе, лазерная вапоризация шейки матки в анамнезе)</span>
+        </label>
+        <select name="two" value={values.two} onChange={handleChange}>
+          <option value={0}>Нет</option>
+          <option value={1}>Да</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label>
+          Инфекционно-воспалительные заболевания и дисбиотические состояния при настоящей беременности
+          <span className="description">(рецидивирующий бактериальный вагиноз, острый вагинит, цервицит, инфекции мочевыводящих путей, бессимптомная бактериурия)</span>
+        </label>
+        <select name="three" value={values.three} onChange={handleChange}>
+          <option value={0}>Нет</option>
+          <option value={1}>Да</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label>
+        Количество лейкоцитов в микроскопическом исследовании отделяемого из цервикального канала в I триместре настоящей беременности 
+          <span className="description">(количество клеток в поле зрения)</span>
+        </label>
+        <input type="number" name="four" value={values.four} onChange={handleChange} className="input-field" required/>
+      </div>
+
+      <button className="calculate-button" onClick={calculate}>Рассчитать</button>
+
+      {result && (
+        <div className="result">
+          <p>Вероятность: <strong>{result}%</strong></p>
+          <p>Результат: <strong>{risk}</strong></p>
+        </div>
+      )}
+    </div>
   );
 }
 
